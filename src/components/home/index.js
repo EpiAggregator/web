f9b9d025be0a4fa0d83c22edfa5038924ea83fd2
moriss_h <moriss_h@epitech.eu>
2017-01-10 01:00:52 +0100

    Make list with rss icon

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,14 @@
 import { h, Component } from 'preact';
 import style from './style';
 
+import { Row, Col } from 'react-grid-system';
+
 import VirtualList from 'preact-virtual-list';
+import TiRss from 'react-icons/lib/ti/rss';
 
 // Generate 100,000 rows of data
 const DATA = [];
-for (let x=1e5; x--; ) DATA[x] = `Item #${x+1}`;
+for (let x=1e2; x--; ) DATA[x] = `Item #${x+1}`;
 
 export default class Home extends Component {
 
@@ -14,18 +17,25 @@ export default class Home extends Component {
 
     // Renders a single row
     renderRow(row) {
-        return <div class="rss-row">{row}</div>;
+        return <div class={style.feedListItem}><TiRss />  {row}</div>;
     }
 
 	render() {
 		return (
 			<div class={style.home}>
-				<h1>Home</h1>
-				<VirtualList sync class="list"
-                data={DATA}
-                rowHeight={this.rowHeight}
-                renderRow={this.renderRow}
-                />
+			<h1>Home</h1>
+			<Col xs={3} >
+                <div class={style.feedListContainer}>
+                    <VirtualList
+                    sync
+                    class={style.feedListTable}
+                    data={DATA}
+                    rowHeight={this.rowHeight}
+                    renderRow={this.renderRow}
+                    />
+                </div>
+			</Col>
+			<Col xs={9} style={{ border: '1px solid silver' }}>Content</Col>
 			</div>
 		);
 	}
