1d398deb184cd84d32b921507a690453c769585b
moriss_h <moriss_h@epitech.eu>
2017-01-22 16:08:55 +0100

    Move from preact + alt to react + redux

--- src/components/loading/index.js
+++
@@ -1,9 +0,0 @@
-import { h, Component } from 'preact';
-import style from './style';
-
-export default class Loading extends Component {
-
-    render() {
-        return (<div class={style.loader}>Loading...</div>);
-    }
-}
