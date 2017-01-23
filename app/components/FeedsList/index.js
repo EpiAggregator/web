6ab53e12ab4407f6d97a9740b93ab659145c30bd
moriss_h <moriss_h@epitech.eu>
2017-01-23 01:02:18 +0100

    Fix faveicon

--- app/components/FeedsList/index.js
+++ app/components/FeedsList/index.js
@@ -22,6 +22,19 @@ const loadStyle = {
     },
 };
 
+function getLocation(href) {
+    var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
+    return match && {
+        protocol: match[1],
+        host: match[2],
+        hostname: match[3],
+        port: match[4],
+        pathname: match[5],
+        search: match[6],
+        hash: match[7]
+    }
+}
+
 // leftIcon={feed.image}
 class FeedsList extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -39,13 +52,14 @@ class FeedsList extends React.Component { // eslint-disable-line react/prefer-st
         </div>);
     if (this.props.feedsList.length < 1)
       return (<div><FormattedMessage {...messages.emptyList} /></div>);
+    let defaultIcon = 'https://perishablepress.com/wp/wp-content/images/2006/feed-collection/feed-icon_orange-128px+.png';
     return (
         <div>
         <List>
             {this.props.feedsList.map((feed, i) =>
                 <ListItem key={i} primaryText={feed.title}
                   leftAvatar={
-                      <Avatar src={feed.image ? feed.image.url : 'https://perishablepress.com/wp/wp-content/images/2006/feed-collection/feed-icon_orange-128px+.png' } />
+                      <Avatar src={feed.link ? (getLocation(feed.link).protocol + '//' + getLocation(feed.link).host + '/favicon.ico') : defaultIcon } />
                   }/>
             )}
         </List>
@@ -57,9 +71,9 @@ class FeedsList extends React.Component { // eslint-disable-line react/prefer-st
 FeedsList.propTypes = {
     feedsList: React.PropTypes.arrayOf(React.PropTypes.shape({
         id: React.PropTypes.string.isRequired,
-        title: React.PropTypes.string.isRequired,
-        description: React.PropTypes.string.isRequired,
-        image: React.PropTypes.object,
+        link: React.PropTypes.string,
+        title: React.PropTypes.string,
+        description: React.PropTypes.string,
     })),
 };
 
