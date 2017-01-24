04ef31d86322cce548a52b72b6626548da48d508
moriss_h <moriss_h@epitech.eu>
2017-01-24 22:15:07 +0100

    CenterDiv components

---
+++ app/components/CenterDiv/index.js
@@ -0,0 +1,16 @@
+/**
+*
+* CenterDiv
+*
+*/
+
+import React from 'react';
+
+import styled from 'styled-components';
+
+const CenterDiv = styled.div`
+width: 50%;
+margin: 0 43%;
+`;
+
+export default CenterDiv;
